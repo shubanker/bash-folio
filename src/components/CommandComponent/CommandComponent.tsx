1a739c72cfddb8e64543aa2ad6b0016c5e31a956
Shubanker Chourasia <subhankerchourasia@gmail.com>
2021-06-26 23:07:30 +0530

    encapsulated command and output into a seperate component.

---
+++ src/components/CommandComponent/CommandComponent.tsx
@@ -0,0 +1,24 @@
+import { useCallback, useState } from "react";
+import { bashUserName } from "../../commands.config";
+import { BashCmd } from "./bashCmd/bashCmd";
+import { CmdOutput } from "./CmdOutput/cmdOutput";
+
+export const CommandComponent: React.FC<{
+  command: Command;
+  next: () => void;
+}> = ({ command, next }) => {
+  const [cmdRendered, setCmdRendered] = useState(false);
+  const setNext = useCallback(() => {
+    next();
+  }, [next]);
+  return (
+    <>
+      <BashCmd
+        userName={bashUserName}
+        cmd={command.command}
+        next={() => setCmdRendered(true)}
+      />
+      {cmdRendered ? <CmdOutput command={command} next={setNext} /> : null}
+    </>
+  );
+};
--- src/components/Folio/Folio.tsx
+++ src/components/Folio/Folio.tsx
@@ -1,67 +1,63 @@
-import React from "react";
+import { useCallback } from "react";
 import { useEffect, useState } from "react";
 import { bashUserName, commands } from "../../commands.config";
-import { BashCmd } from "../bashCmd/bashCmd";
-import { CmdOutput } from "../CmdOutput/cmdOutput";
+import { BashCmd } from "../CommandComponent/bashCmd/bashCmd";
+import { CommandComponent } from "../CommandComponent/CommandComponent";
 import classes from "./Folio.module.scss";
 
 export const Folio = () => {
-  //number of rendered commands
-  const [commandCount, setCommandCount] = useState(0);
-  //Track of index for whom command typing is done, now show result.
-  const [typedIndex, setTypedIndex] = useState(new Set<number>());
+  //commands rendered.
+  const [processedCommands, setProcessedCommands] = useState([] as Command[]);
   //To show empty shell input at end.
   const [inProgress, setInProgress] = useState(false);
+  const [scrollIntervalRef, setScrollIntervalRef] = useState(null as any);
 
-  const [scrollInterval, setScrollInterval] = useState(null as any);
-
-  //start the loop.
-  useEffect(() => {
-    setTimeout(() => {
-      setCommandCount(1);
-      setInProgress(true);
-
-      //auto-Scroll to bottom until all commands are rendered
-      setScrollInterval(
-        setInterval(() => {
-          window.scrollTo({
-            behavior: "smooth",
-            top: document.body.scrollHeight,
-          });
-        }, 2000)
-      );
-    }, 400);
+  const scrollToBottom = useCallback(() => {
+    window.scrollTo({
+      behavior: "smooth",
+      top: document.body.scrollHeight,
+    });
   }, []);
 
   //Command finished rendering, wait a while and continue with next.
-  const prepareNext = () => {
+  const prepareNext = useCallback(() => {
     setInProgress(false);
     setTimeout(() => {
-      if (commandCount < commands.length) {
-        setCommandCount(commandCount + 1);
+      if (processedCommands.length < commands.length) {
+        const withNextCommand = [
+          ...processedCommands,
+          commands[processedCommands.length],
+        ];
+        setProcessedCommands(withNextCommand);
         setInProgress(true);
       } else {
-        clearInterval(scrollInterval);
+        scrollToBottom();
+        clearInterval(scrollIntervalRef);
       }
     }, 400);
-  };
+  }, [processedCommands, scrollToBottom, scrollIntervalRef]);
 
-  const afterCmdTyped = (index: number) => {
-    const newSet = new Set(typedIndex);
-    newSet.add(index);
-    setTypedIndex(newSet);
-  };
-  const commandJsx = commands.slice(0, commandCount).map((command, index) => (
-    <React.Fragment key={index}>
-      <BashCmd
-        userName={bashUserName}
-        cmd={command.command}
-        next={() => afterCmdTyped(index)}
-      />
-      {typedIndex.has(index) ? (
-        <CmdOutput command={command} next={prepareNext} />
-      ) : null}
-    </React.Fragment>
+  //need to pass prepareNext only to command currently being rendered,
+  // for pre-rendered ones pass a dummy next.
+  const dummyFn = useCallback(() => {}, []);
+
+  //start the loop.
+  useEffect(() => {
+    prepareNext();
+    //auto-Scroll to bottom until all commands are rendered
+    setScrollIntervalRef(
+      setInterval(() => {
+        scrollToBottom();
+      }, 2000)
+    );
+  }, [scrollToBottom]);
+
+  const commandJsx = processedCommands.map((command, index) => (
+    <CommandComponent
+      key={index}
+      command={command}
+      next={index === processedCommands.length - 1 ? prepareNext : dummyFn}
+    />
   ));
   return (
     <div className={`container ${classes.container}`}>

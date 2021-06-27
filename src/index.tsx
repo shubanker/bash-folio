bbe7f07954165af49614feda990c3dac34fa2570
Shubanker Chourasia <subhankerchourasia@gmail.com>
2021-06-27 23:14:37 +0530

    moved folio directly in index

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Folio } from "./components/Folio/Folio";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Folio />
   </React.StrictMode>,
   document.getElementById("root")
 );
